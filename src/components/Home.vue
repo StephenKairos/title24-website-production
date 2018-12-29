fd9f832a249fa355cc9c87a236bb534e1b3990c5
StephenKairos <stephenkairos.reyes@sjsu.edu>
2018-12-28 16:49:40 -0800

    testing form submit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -75,7 +75,7 @@ export default {
     onSubmit (evt) {
       evt.preventDefault();
       // alert(JSON.stringify(this.form));
-      alert("Thank you for your request! We will get back to you shortly.");
+      //alert("Thank you for your request! We will get back to you shortly.");
 
       sendEmail();
     },
@@ -92,13 +92,10 @@ export default {
     },
     async sendEmail() {
       await EmailService.sendEmail({
-        form: {
-          name: this.form.name,
-          email: this.form.email,
-          number: this.form.number,
-          text: this.form.text
-        }
+        name: this.form.name
       });
+
+      alert(this.form.name);
     }
   }
 }
