879c8cfc15c93979b87fef2f73fc55be7e260819
StephenKairos <stephenkairos.reyes@sjsu.edu>
2019-01-10 13:12:42 -0800

    npm deploy

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -143,32 +143,33 @@
       <b-row>
         <b-col md="12">
           <h2>Energy compliance report specialists at your service</h2>
-          <p>We have been preparing approved Title 24 calculations and reports for Architects, Contractors, Engineers, Designers and Homeowners for over a decade. The experience, skill, and quality of service that we have provided over these years make us the expert and go to consultants in the field. The moment you call or email us, one of our qualified energy specialists will work with you closely until you get the guaranteed approval of your report by the city. All of these will be accomplished within the shortest amount of time, normally within hours and not days. You will find that we have the most affordable rate in the industry that you can find. We have everything you need for a seamless process in complying with the strict California Building Energy Efficiency standards:
+          <p>We have been preparing approved Title 24 calculations and reports for Architects, Contractors, Engineers, Designers and Homeowners for over a decade. The experience, skill, and quality of service that we have provided over these years make us the expert and go to consultants in the field. The moment you call or email us, one of our qualified energy specialists will work with you closely until you get the guaranteed approval of your report by the city. All of these will be accomplished within the shortest amount of time, normally within hours and not days. You will find that we have the most affordable rate in the industry that you can find. We have everything you need for a seamless process in complying with the strict California Building Energy Efficiency standards:</p>
 
-          We serve all counties and cities in California. We communicate diligently with each of our client to understand their specific need in order to minimize and eliminate project risk. Come and join our satisfied and happy customers, many of who whom have become our repeat clients over the years due to our bar none service and exemplary delivered results. Others have gone to give us referrals, reflecting the quality of trust that have been built over these years with them.
+          <p>We serve all counties and cities in California. We communicate diligently with each of our client to understand their specific need in order to minimize and eliminate project risk. Come and join our satisfied and happy customers, many of who whom have become our repeat clients over the years due to our bar none service and exemplary delivered results. Others have gone to give us referrals, reflecting the quality of trust that have been built over these years with them.</p>
 
-          WHAT YOU CAN EXPECT FROM US:
+          <p>WHAT YOU CAN EXPECT FROM US:</p>
 
-          Fast turnaround, which is typically within 24 -48 hours.
-          Guaranteed approved compliance report by the building department through a quick plan-check process.
-          You will be provided with energy compliance options in order to save you thousands of dollars in project costs.
-          You will be guided through the step-by-step approval process and be advised on city specific requirements.
+          <b-list-group>
+            <b-list-group-item style="text-align: justify;"><strong>Fast turnaround, which is typically within 24 -48 hours.</strong></b-list-group-item>
+            <b-list-group-item style="text-align: justify;"><strong>Guaranteed approved compliance report by the building department through a quick plan-check process.</strong></b-list-group-item>
+            <b-list-group-item style="text-align: justify;"><strong>You will be provided with energy compliance options in order to save you thousands of dollars in project costs.</strong></b-list-group-item>
+            <b-list-group-item style="text-align: justify;"><strong>You will be guided through the step-by-step approval process and be advised on city specific requirements.</strong></b-list-group-item>
           
-          Energy Calculations
+          <p>Energy Calculations</p>
 
-          When California experienced one of its energy crisis, new regulations for energy conservation standards for all of the new residential and new nonresidential buildings in 1977 were established and adopted by the California Energy Commission, which became effective in July of 1978. These would also include additions and alterations to existing buildings be subject to meet the minimum standards of the California Code of Regulations. Through the years, amendments were made and building regulations were re-adopted whichwill remain in effect with an ongoing strict and technical rigidity.
+          <p>When California experienced one of its energy crisis, new regulations for energy conservation standards for all of the new residential and new nonresidential buildings in 1977 were established and adopted by the California Energy Commission, which became effective in July of 1978. These would also include additions and alterations to existing buildings be subject to meet the minimum standards of the California Code of Regulations. Through the years, amendments were made and building regulations were re-adopted whichwill remain in effect with an ongoing strict and technical rigidity.</p>
 
-          Due to these standards, sustaining natural gas and electricity has been contributory in preventing the state to construct more power plants. According to the California Energy Commission, “due to the energy efficiency standards, Californians have saved more than $74 billion in reduced electricity bills since 1977.
+          <p>Due to these standards, sustaining natural gas and electricity has been contributory in preventing the state to construct more power plants. According to the California Energy Commission, “due to the energy efficiency standards, Californians have saved more than $74 billion in reduced electricity bills since 1977.</p>
 
-          As a result of the process in calculating the energy savings correlated to a more efficient house or building and with the process of replacing old appliances with newer and more efficient ones as well as replacing old buildings with new architecture, California’s per capita electricity use remain flat over the years while the use of the rest of the country continue to accelerate.”
+          <p>As a result of the process in calculating the energy savings correlated to a more efficient house or building and with the process of replacing old appliances with newer and more efficient ones as well as replacing old buildings with new architecture, California’s per capita electricity use remain flat over the years while the use of the rest of the country continue to accelerate.”</p>
 
-          Compliance Report Process
+          <p>Compliance Report Process</p>
 
-          We only have one mission and that is for you to get approved quickly and cost-effectively. Once you send us your project drawings either through email, website submission or through express delivery, we will start the work right away. Sometimes, client wants to meet us in person specially if they are within close proximity to our location and we would be glad to meet with them. Once the report is completed, it will be signed and delivered to you in PDF format. This is now ready for submission to the building department in the city that you are working with. Through each progress, we will be available to answer any questions, clarifications and updates from you.
+          <p>We only have one mission and that is for you to get approved quickly and cost-effectively. Once you send us your project drawings either through email, website submission or through express delivery, we will start the work right away. Sometimes, client wants to meet us in person specially if they are within close proximity to our location and we would be glad to meet with them. Once the report is completed, it will be signed and delivered to you in PDF format. This is now ready for submission to the building department in the city that you are working with. Through each progress, we will be available to answer any questions, clarifications and updates from you.</p>
 
-          Our Satisfied Clients
+          <p>Our Satisfied Clients</p>
 
-          We value each client and service them with utmost competence. Knowing that this crucial part of the process determines the future of the project at hand, we know where our customers are coming from. And since many of them have come for us on their subsequent projects, we are proud to share with you some of their testimonials.</p>
+          <p>We value each client and service them with utmost competence. Knowing that this crucial part of the process determines the future of the project at hand, we know where our customers are coming from. And since many of them have come for us on their subsequent projects, we are proud to share with you some of their testimonials.</p>
         </b-col>
       </b-row>
     </div>
