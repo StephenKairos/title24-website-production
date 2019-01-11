d86e5a9d532af9750e91c72aa53b51cf5437feb7
StephenKairos <stephenkairos.reyes@sjsu.edu>
2019-01-10 21:44:24 -0800

    npm deploy

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -89,7 +89,7 @@
       <b-row class="section-color">
         <b-col class="column-padding" md="3">
           <h3>let us know</h3>
-          <img src="../assets/order-plans.jpg"/>
+          <img class="rounded-circle" src="../assets/contactus.jpg"/>
           <div>
             <b-btn v-b-toggle="'collapse1'" variant="primary">Find out more...</b-btn>
             <b-collapse id="collapse1" class="mt-2">
@@ -101,7 +101,7 @@
         </b-col>
         <b-col class="column-padding" md="3">
           <h3>email/upload plans</h3>
-          <img src="../assets/order-plans.jpg"/>
+          <img class="rounded-circle" src="../assets/blueprint.jpg"/>
           <div>
             <b-btn v-b-toggle="'collapse2'" variant="primary">Find out more...</b-btn>
             <b-collapse id="collapse2" class="mt-2">
@@ -113,7 +113,7 @@
         </b-col>
         <b-col class="column-padding" md="3">
           <h3>receive Title 24 report</h3>
-          <img src="../assets/order-plans.jpg"/>
+          <img class="rounded-circle" src="../assets/computerfiles.jpg"/>
           <div>
             <b-btn v-b-toggle="'collapse3'" variant="primary">Find out more...</b-btn>
             <b-collapse id="collapse3" class="mt-2">
@@ -125,7 +125,7 @@
         </b-col>
         <b-col class="column-padding" md="3">
           <h3>print 2 copies</h3>
-          <img src="../assets/order-plans.jpg"/>
+          <img class="rounded-circle" src="../assets/foreman.jpg"/>
           <div>
             <b-btn v-b-toggle="'collapse4'" variant="primary">Find out more...</b-btn>
             <b-collapse id="collapse4" class="mt-2">
@@ -147,15 +147,18 @@
 
           <p>We serve all counties and cities in California. We communicate diligently with each of our client to understand their specific need in order to minimize and eliminate project risk. Come and join our satisfied and happy customers, many of who whom have become our repeat clients over the years due to our bar none service and exemplary delivered results. Others have gone to give us referrals, reflecting the quality of trust that have been built over these years with them.</p>
 
-          <p>WHAT YOU CAN EXPECT FROM US:</p>
+          <p><strong>WHAT YOU CAN EXPECT FROM US:</strong></p>
 
           <b-list-group>
             <b-list-group-item style="text-align: justify;"><strong>Fast turnaround, which is typically within 24 -48 hours.</strong></b-list-group-item>
             <b-list-group-item style="text-align: justify;"><strong>Guaranteed approved compliance report by the building department through a quick plan-check process.</strong></b-list-group-item>
             <b-list-group-item style="text-align: justify;"><strong>You will be provided with energy compliance options in order to save you thousands of dollars in project costs.</strong></b-list-group-item>
             <b-list-group-item style="text-align: justify;"><strong>You will be guided through the step-by-step approval process and be advised on city specific requirements.</strong></b-list-group-item>
-          
-          <p>Energy Calculations</p>
+          </b-list-group>
+
+          <br/>
+
+          <p><strong>Energy Calculations</strong></p>
 
           <p>When California experienced one of its energy crisis, new regulations for energy conservation standards for all of the new residential and new nonresidential buildings in 1977 were established and adopted by the California Energy Commission, which became effective in July of 1978. These would also include additions and alterations to existing buildings be subject to meet the minimum standards of the California Code of Regulations. Through the years, amendments were made and building regulations were re-adopted whichwill remain in effect with an ongoing strict and technical rigidity.</p>
 
@@ -163,11 +166,11 @@
 
           <p>As a result of the process in calculating the energy savings correlated to a more efficient house or building and with the process of replacing old appliances with newer and more efficient ones as well as replacing old buildings with new architecture, California’s per capita electricity use remain flat over the years while the use of the rest of the country continue to accelerate.”</p>
 
-          <p>Compliance Report Process</p>
+          <p><strong>Compliance Report Process</strong></p>
 
           <p>We only have one mission and that is for you to get approved quickly and cost-effectively. Once you send us your project drawings either through email, website submission or through express delivery, we will start the work right away. Sometimes, client wants to meet us in person specially if they are within close proximity to our location and we would be glad to meet with them. Once the report is completed, it will be signed and delivered to you in PDF format. This is now ready for submission to the building department in the city that you are working with. Through each progress, we will be available to answer any questions, clarifications and updates from you.</p>
 
-          <p>Our Satisfied Clients</p>
+          <p><strong>Our Satisfied Clients</strong></p>
 
           <p>We value each client and service them with utmost competence. Knowing that this crucial part of the process determines the future of the project at hand, we know where our customers are coming from. And since many of them have come for us on their subsequent projects, we are proud to share with you some of their testimonials.</p>
         </b-col>
@@ -245,7 +248,7 @@ export default {
 }
 
 .text-section p {
-  text-align: left;
+  text-align: justify;
 }
 
 .subtitle-color {
